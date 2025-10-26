1695382b81298af09a1a2884548c41993f1426b0
lyrikx <cristianjosue0505@gmail.com>
2025-10-25 21:32:12 -0400

    feat: translate NovaSidebar class and messages to English

--- novaSidebar.js
+++ novaSidebar.js
@@ -1,4 +1,4 @@
-const arrowSvg = `<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15px" height="15px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve" transform="rotate(180)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M33.934,54.458l30.822,27.938c0.383,0.348,0.864,0.519,1.344,0.519c0.545,0,1.087-0.222,1.482-0.657 c0.741-0.818,0.68-2.083-0.139-2.824L37.801,52.564L64.67,22.921c0.742-0.818,0.68-2.083-0.139-2.824 c-0.817-0.742-2.082-0.679-2.824,0.139L33.768,51.059c-0.439,0.485-0.59,1.126-0.475,1.723 C33.234,53.39,33.446,54.017,33.934,54.458z"></path> </g> </g></svg>`
+const arrowSvg = `<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15px" height="15px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve" transform="rotate(180)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M33.934,54.458l30.822,27.938c0.383,0.348,0.864,0.519,1.344,0.519c0.545,0,1.087-0.222,1.482-0.657 c0.741-0.818,0.68-2.083-0.139-2.824L37.801,52.564L64.67,22.921c0.742-0.818,0.68-2.083-0.139-2.824 c-0.817-0.742-2.082-0.679-2.824,0.139L33.768,51.059c-0.439,0.485-0.59,1.126-0.475,1.723 C33.234,53.39,33.446,54.017,33.934,54.458z"></path> </g> </g></svg>`;
 
 export class NovaSidebar {
 
@@ -6,7 +6,7 @@ export class NovaSidebar {
 
 		this.container = document.querySelector(containerSelector);
 		
-		if (!this.container) throw new Error(`NovaSidebar: No se encontró el contenedor ${containerSelector}`);
+		if (!this.container) throw new Error(`NovaSidebar: Container ${containerSelector} not found`);
 
 		this.config = config;
 		this.mode = options.mode || 'spa';
@@ -21,7 +21,6 @@ export class NovaSidebar {
 		this.currentActive = null;
 
 		this.render();
-
 	}
 
 	render() {
